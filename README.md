2df8a036a3524d736395ae4b7ca961f841ed60c9
riku1227 <17927874+riku1227@users.noreply.github.com>
2020-03-15 21:31:23 +0900

    :tada: Source code

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ Operation check version: 1.6.3 , 1.7.0
 動作確認済バージョン: 1.6.3, 1.7.0
 
 ## [Download](https://github.com/riku1227/BSQuest-QuestDarthMaul/releases)
-  
-Source code is soon...? sorry  
-ソースコードもリリース予定です(すぐに公開できなくて申し訳ないです)
+
+## Credits
+
+* [Sc2ad](https://github.com/Sc2ad) and [jakibaki](https://github.com/jakibaki) - [beatsaber-hook](https://github.com/sc2ad/beatsaber-hook)
+* [raftario](https://github.com/raftario) - [vscode-bsqm](https://github.com/raftario/vscode-bsqm) and [this template](https://github.com/raftario/bmbf-mod-template)
+* [Kyle1413](https://github.com/Kylemc1413/BSDarthMaul) - [BSDarthMaul](https://github.com/Kylemc1413/BSDarthMaul)
---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <sstream>
+
+#include "../include/main.hpp"
+#include "../extern/beatsaber-hook/shared/utils/il2cpp-functions.hpp"
+#include "../extern/beatsaber-hook/shared/utils/typedefs.h"
+
+MAKE_HOOK_OFFSETLESS(PlayerController_Update, void, Il2CppObject* self) {
+    PlayerController_Update(self);
+
+    Il2CppObject* leftSaber = il2cpp_utils::GetFieldValue(self, "_leftSaber");
+    Il2CppObject* rightSaber = il2cpp_utils::GetFieldValue(self, "_rightSaber");
+
+    if(leftSaber != nullptr && rightSaber != nullptr) {
+        log(INFO, "Saber's online");
+        Il2CppObject* leftSaberTransform = nullptr;
+        Il2CppObject* rightSaberTransform = nullptr;
+
+        Il2CppClass* componentsClass = il2cpp_utils::GetClassFromName("", "Saber");
+        il2cpp_utils::RunMethod(&leftSaberTransform, leftSaber, il2cpp_functions::class_get_method_from_name(componentsClass, "get_transform", 0));
+        il2cpp_utils::RunMethod(&rightSaberTransform, rightSaber, il2cpp_functions::class_get_method_from_name(componentsClass, "get_transform", 0));
+
+        if(leftSaberTransform != nullptr && rightSaberTransform != nullptr) {
+
+            Il2CppClass* transformClass = il2cpp_utils::GetClassFromName("UnityEngine", "Transform");
+            const MethodInfo* getMethod = il2cpp_functions::class_get_method_from_name(transformClass, "get_localPosition", 0);
+            const MethodInfo* setMethod = il2cpp_functions::class_get_method_from_name(transformClass, "set_localPosition", 1);
+            const MethodInfo* setRotate = il2cpp_functions::class_get_method_from_name(transformClass, "Rotate", 1);
+            const MethodInfo* setTranslate = il2cpp_functions::class_get_method_from_name(transformClass, "Translate", 1);
+            const MethodInfo* getLocalRotation = il2cpp_functions::class_get_method_from_name(transformClass, "get_localRotation", 0);
+            const MethodInfo* setLocalRotation = il2cpp_functions::class_get_method_from_name(transformClass, "set_localRotation", 1);
+
+            Vector3 rightSaberLocalPosition;
+            il2cpp_utils::RunMethod(&rightSaberLocalPosition, rightSaberTransform, getMethod);
+            Quaternion rightSaberLocalRotation;
+            il2cpp_utils::RunMethod(&rightSaberLocalRotation, rightSaberTransform, getLocalRotation);
+
+            il2cpp_utils::RunMethod(leftSaberTransform, setMethod, rightSaberLocalPosition);
+            il2cpp_utils::RunMethod(leftSaberTransform, setLocalRotation, rightSaberLocalRotation);
+            il2cpp_utils::RunMethod(leftSaberTransform, setRotate, Vector3{ 0, 180, 180});
+            il2cpp_utils::RunMethod(leftSaberTransform, setTranslate, Vector3{0, 0, 0.335});
+        }
+    }
+}
+
+__attribute__((constructor)) void lib_main()
+{
+    log(INFO, "Hello from the first time this mod is loaded!");
+}
+
+extern "C" void load() {
+    log(INFO, "Hello from il2cpp_init!");
+    log(INFO, "Installing hooks...");
+    INSTALL_HOOK_OFFSETLESS(PlayerController_Update, il2cpp_utils::FindMethod("", "PlayerController", "Update", 0));
+    log(INFO, "Installed all hooks!");
+}
