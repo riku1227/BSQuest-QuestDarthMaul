616068096f4335272c4c257c48ae98e319545c5a
henwill8 <43818215+henwill8@users.noreply.github.com>
2020-03-24 13:18:25 -0600

    Adding my code

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,118 @@
-#include <string>
-#include <sstream>
+#include "../include/main.hpp"
+#include "../extern/beatsaber-hook/shared/customui/customui.hpp"
 
-#include <dlfcn.h>
-#include "../extern/beatsaber-hook/shared/utils/utils.h"
-#include "../extern/beatsaber-hook/shared/utils/il2cpp-functions.hpp"
-#include "../extern/beatsaber-hook/shared/utils/typedefs.h"
-                                                                                                                                   
+int stop = 0;
+
+CustomUI::TextObject Maul;
+
+float grayScaleColor = 1.0f;
+
+Vector2 posOffset = {-41.3f,-16.9f};
+
+float fontSize = 3.6f;
+
+bool leftHand;
+
+bool noArrows;
+
+MAKE_HOOK_OFFSETLESS(Refresh, void, Il2CppObject* self) {
+    if(stop == 1) {
+        Il2CppObject* nameText = il2cpp_utils::GetFieldValue(self, "_nameText");
+	    Il2CppObject *nameTextTransform;
+	    Il2CppObject *nameTextParent;
+
+        il2cpp_utils::RunMethod(&nameTextTransform, nameText, "get_transform");
+	    il2cpp_utils::RunMethod(&nameTextParent, nameTextTransform, "GetParent");
+
+        Maul.text = "/Darth Maul Mode";
+	    Maul.fontSize = fontSize;
+	    Maul.parentTransform = nameTextParent;
+        Maul.sizeDelta = posOffset;
+        Maul.color = {grayScaleColor,grayScaleColor,grayScaleColor,grayScaleColor};
+	    Maul.create();
+    }
+    stop++;
+
+    Refresh(self);
+}
+
+MAKE_HOOK_OFFSETLESS(SongStart, void, Il2CppObject* self, Il2CppObject* difficultyBeatmap, Il2CppObject* overrideEnvironmentSettings, Il2CppObject* overrideColorScheme, Il2CppObject* gameplayModifiers, Il2CppObject* playerSpecificSettings, Il2CppObject* practiceSettings, Il2CppString* backButtonText, bool useTestNoteCutSoundEffects) {
+
+    il2cpp_utils::RunMethod(&noArrows, gameplayModifiers, "get_noArrows");
+
+    il2cpp_utils::RunMethod(&leftHand, playerSpecificSettings, "get_leftHanded");
+    
+    SongStart(self, difficultyBeatmap, overrideEnvironmentSettings, overrideColorScheme, gameplayModifiers, playerSpecificSettings, practiceSettings, backButtonText, useTestNoteCutSoundEffects);
+}
+
 MAKE_HOOK_OFFSETLESS(PlayerController_Update, void, Il2CppObject* self) {
+
     PlayerController_Update(self);
 
-    Il2CppObject* leftSaber = il2cpp_utils::GetFieldValue(self, "_leftSaber");
-    Il2CppObject* rightSaber = il2cpp_utils::GetFieldValue(self, "_rightSaber");
+    if(noArrows) {
+        Il2CppObject* leftSaber = il2cpp_utils::GetFieldValue(self, "_leftSaber");
+        Il2CppObject* rightSaber = il2cpp_utils::GetFieldValue(self, "_rightSaber");
 
-    if(leftSaber != nullptr && rightSaber != nullptr) {
-        log(INFO, "Saber's online");
-        Il2CppObject* leftSaberTransform = nullptr;
-        Il2CppObject* rightSaberTransform = nullptr;
+        if(leftSaber != nullptr && rightSaber != nullptr) {
+            log(INFO, "Saber's online");
+            Il2CppObject* leftSaberTransform = nullptr;
+            Il2CppObject* rightSaberTransform = nullptr;
 
-        Il2CppClass* componentsClass = il2cpp_utils::GetClassFromName("", "Saber");
-        il2cpp_utils::RunMethod(&leftSaberTransform, leftSaber, il2cpp_functions::class_get_method_from_name(componentsClass, "get_transform", 0));
-        il2cpp_utils::RunMethod(&rightSaberTransform, rightSaber, il2cpp_functions::class_get_method_from_name(componentsClass, "get_transform", 0));
+            Il2CppClass* componentsClass = il2cpp_utils::GetClassFromName("", "Saber");
+            il2cpp_utils::RunMethod(&leftSaberTransform, leftSaber, il2cpp_functions::class_get_method_from_name(componentsClass, "get_transform", 0));
+            il2cpp_utils::RunMethod(&rightSaberTransform, rightSaber, il2cpp_functions::class_get_method_from_name(componentsClass, "get_transform", 0));
 
-        if(leftSaberTransform != nullptr && rightSaberTransform != nullptr) {
-
-            Il2CppClass* transformClass = il2cpp_utils::GetClassFromName("UnityEngine", "Transform");
-            const MethodInfo* getMethod = il2cpp_functions::class_get_method_from_name(transformClass, "get_localPosition", 0);
-            const MethodInfo* setMethod = il2cpp_functions::class_get_method_from_name(transformClass, "set_localPosition", 1);
-            const MethodInfo* setRotate = il2cpp_functions::class_get_method_from_name(transformClass, "Rotate", 1);
-            const MethodInfo* setTranslate = il2cpp_functions::class_get_method_from_name(transformClass, "Translate", 1);
-            const MethodInfo* getLocalRotation = il2cpp_functions::class_get_method_from_name(transformClass, "get_localRotation", 0);
-            const MethodInfo* setLocalRotation = il2cpp_functions::class_get_method_from_name(transformClass, "set_localRotation", 1);
+            if(leftSaberTransform != nullptr && rightSaberTransform != nullptr) {
+                Il2CppClass* transformClass = il2cpp_utils::GetClassFromName("UnityEngine", "Transform");
+                const MethodInfo* getMethod = il2cpp_functions::class_get_method_from_name(transformClass, "get_localPosition", 0);
+                const MethodInfo* setMethod = il2cpp_functions::class_get_method_from_name(transformClass, "set_localPosition", 1);
+                const MethodInfo* setRotate = il2cpp_functions::class_get_method_from_name(transformClass, "Rotate", 1);
+                const MethodInfo* setTranslate = il2cpp_functions::class_get_method_from_name(transformClass, "Translate", 1);
+                const MethodInfo* getLocalRotation = il2cpp_functions::class_get_method_from_name(transformClass, "get_localRotation", 0);
+                const MethodInfo* setLocalRotation = il2cpp_functions::class_get_method_from_name(transformClass, "set_localRotation", 1);
 
                 Vector3 rightSaberLocalPosition;
+                Vector3 leftSaberLocalPosition;
                 il2cpp_utils::RunMethod(&rightSaberLocalPosition, rightSaberTransform, getMethod);
+                il2cpp_utils::RunMethod(&leftSaberLocalPosition, leftSaberTransform, getMethod);
                 Quaternion rightSaberLocalRotation;
-            il2cpp_utils::RunMethod(&rightSaberLocalRotation, rightSaberTransform, getLocalRotation);
+                Quaternion leftSaberLocalRotation;
+                il2cpp_utils::RunMethod(&rightSaberLocalRotation, rightSaberTransform, getLocalRotation);
+                il2cpp_utils::RunMethod(&leftSaberLocalRotation, leftSaberTransform, getLocalRotation);
 
-            il2cpp_utils::RunMethod(leftSaberTransform, setMethod, rightSaberLocalPosition);
-            il2cpp_utils::RunMethod(leftSaberTransform, setLocalRotation, rightSaberLocalRotation);
-            il2cpp_utils::RunMethod(leftSaberTransform, setRotate, Vector3{ 0, 180, 0});
-            il2cpp_utils::RunMethod(leftSaberTransform, setTranslate, Vector3{0, 0, 0.335});
+                if(!leftHand) {
+                    il2cpp_utils::RunMethod(leftSaberTransform, setMethod, rightSaberLocalPosition);
+                    il2cpp_utils::RunMethod(leftSaberTransform, setLocalRotation, rightSaberLocalRotation);
+                    il2cpp_utils::RunMethod(leftSaberTransform, setRotate, Vector3{ 0, 180, 0});
+                    il2cpp_utils::RunMethod(leftSaberTransform, setTranslate, Vector3{0, 0, 0.335});
+                } else {
+                    il2cpp_utils::RunMethod(rightSaberTransform, setMethod, leftSaberLocalPosition);
+                    il2cpp_utils::RunMethod(rightSaberTransform, setLocalRotation, leftSaberLocalRotation);
+                    il2cpp_utils::RunMethod(rightSaberTransform, setRotate, Vector3{ 0, 180, 0});
+                    il2cpp_utils::RunMethod(rightSaberTransform, setTranslate, Vector3{0, 0, 0.335});
+                }
+            }
         }
     }
 }
 
-// Haptic Remapping (gives errors, Going too need a UnityEngine.XR Replica)
 MAKE_HOOK_OFFSETLESS(HapticFeedbackController_HitNote, void, Il2CppObject* self, int node) {
-    //HapticFeedbackController_HitNote(self , node);
-    il2cpp_utils::RunMethod(self, "Rumble" ,5, 0.13f, 1.0f, 0.0f);
+    if(noArrows) {
+        if(!leftHand) {
+            node = 5;
+        } else {
+            node = 4;
+        }
+    }
+    HapticFeedbackController_HitNote(self, node);
 }
 
-
-
-__attribute__((constructor)) void lib_main()
-{
-    log(INFO, "Hello from the first time this mod is loaded!");
-}
 
 extern "C" void load() {
-    log(INFO, "Hello from il2cpp_init!");
     log(INFO, "Installing hooks...");
-    INSTALL_HOOK_OFFSETLESS(PlayerController_Update, il2cpp_utils::FindMethod("", "PlayerController", "Update", 0));
-    INSTALL_HOOK_OFFSETLESS(HapticFeedbackController_HitNote, il2cpp_utils::FindMethod("", "HapticFeedbackController", "HitNote", 1));
+    INSTALL_HOOK_OFFSETLESS(Refresh, il2cpp_utils::FindMethodUnsafe("", "GameplayModifierToggle", "Start", 0));
+    INSTALL_HOOK_OFFSETLESS(SongStart, il2cpp_utils::FindMethodUnsafe("", "StandardLevelScenesTransitionSetupDataSO", "Init", 8));
+    INSTALL_HOOK_OFFSETLESS(PlayerController_Update, il2cpp_utils::FindMethodUnsafe("", "PlayerController", "Update", 0));
+    INSTALL_HOOK_OFFSETLESS(HapticFeedbackController_HitNote, il2cpp_utils::FindMethodUnsafe("", "HapticFeedbackController", "HitNote", 1));
     log(INFO, "Installed all hooks!");
 }
